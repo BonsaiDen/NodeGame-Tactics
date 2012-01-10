730306cc1012a7c94a910e11fdaf3891aeefa495
Ivo Wetzel <ivo.wetzel@googlemail.com>
2012-01-10 19:06:15 +0100

    Clean up Twist

--- client/lib/Twist.js
+++ client/lib/Twist.js
@@ -26,93 +26,36 @@ var Twist = Class({
         defaultRenderFps: 60
     },
 
+    /**
+      * {Twist} A game loop with two distinct timers for decoupled
+      * @updateFps {Integer} and @renderFps {Integer} rates.
+      */
     constructor: function(updateFps, renderFps) {
 
-        this.__currentFps = {};
-        this.__currentLoad = {};
-        this.__renderInterpolation = false;
+        this._currentFps = {};
+        this._currentLoad = {};
+        this._renderInterpolation = false;
 
-        this.__reset();
-        this.setFps(updateFps || 30, renderFps || 60);
+        this._reset();
+        this.setFps(updateFps || Twist.defaultUpdateFps,
+                    renderFps || Twist.defaultRenderFps);
 
-        this.__intervalId = -1;
-        this.__useAverage = true;
-
-    },
-
-    /**
-      *  Set the desired maximum frame rate for the update and render loop.
-      *
-      *  @param {Number} updateFps The desired logic updates per second.
-      *  @param {Number} rendeerFps The desired renderings per second.
-      */
-    setFps: function(updateFps, renderFps) {
-
-        this.__updateFps = updateFps;
-        this.__renderFps = renderFps;
-
-        // We floor here so that we ensure the desired framerate (we're capping)
-        this.__updateDuration = Math.ceil(1000 / updateFps);
-        this.__renderDuration = Math.floor(1000 / renderFps);
-        this.__tickDuration = Math.max(this.__updateDuration, this.__renderDuration);
+        this._frameCap = false;
+        this._intervalId = -1;
+        this._useAverage = true;
 
     },
 
     /**
-      *  Returns the Fps within the last second.
+      * {Boolean} Starts the game loop and returns `true` in case it wasn't
+      * already running
       *
-      *  @returns {Object} { update: {Number}, render: {Number} }
-      */
-    getFps: function() {
-        return this.__currentFps;
-    },
-
-
-    /**
-      *  Enable or disable the render interpolation
-      */
-    setRenderInterpolation: function(mode) {
-        this.__renderInterpolation = mode;
-    },
-
-    /**
-      *  Returns the load factor within the last second.
-      *
-      *  @returns {Object} { update: {Number}, render: {Number} }
-      */
-    getLoad: function() {
-        return this.__currentLoad;
-    },
-
-    /**
-      *  Returns current update time in milliseconds.
-      *
-      *  Resolution is dependent on the frame rate unless smooth is set to true.
-      *
-      *  @param {Boolean} smooth If true, the time has a higher resolution but it
-      *                          does not necessarily coresspond to the time passed
-      *                          into the "update" function.
-      *
-      *  @returns {Number} The current update time in milliseconds.
-      */
-    getTime: function(smooth) {
-        return smooth ? this.__passedUpdateTime : this.__updateTime;
-    },
-
-    /**
-      *  Starts the loop.
-      *
-      *  In case the loop is already running, you'll have to stop it first.
-      *
-      *  @param {HTMLElement} element An optional element used for the
-      *                               internal window.requestAnimationFrame
-      *                               ticker. Default to 'document.body'.
-      *
-      *  @returns {Boolean} True in case the loop was started.
+      * In case it's desired to sync the drawing with a specific {HTMLElement},
+      * supply it via @element {HTMLElement}.
       */
     start: function(element) {
 
-        if (this.__isRunning) {
+        if (this._isRunning) {
             return false;
         }
 
@@ -123,142 +66,207 @@ var Twist = Class({
         function frameTick() {
 
             // Drop out in case we're not running anymore
-            if (!that.__isRunning) {
+            if (!that._isRunning) {
                 return;
             }
 
-            that.__lastFrameTime = Date.now();
-            that.__tick(true);
-            window.requestAnimFrame(frameTick, element);
+            that._lastFrameTime = Date.now();
+            that._tick(true);
+            window.twistAnimationFrame(frameTick, element);
 
         }
 
         // Handle the low resolution ticker which makes
         // sure that we don't loose to much game state in cases
         // where the browser throttles requestAnimationFrame
-        var tickIndex = 0;
         function intervalTick() {
 
             // Drop out in case we're not running anymore
-            if (!that.__isRunning) {
+            if (!that._isRunning) {
                 return;
             }
             // Ensure that the updates keep being performed in cases where requestAnimationFrame is throttled
-            if (Date.now() - that.__lastFrameTime > that.__tickDuration) {
-                that.__tick(tickIndex === 1);
+            if (Date.now() - that._lastFrameTime > that._tickDuration) {
+                that._tick();
             }
 
-            // Used for ticking updates:render with a 2:1 ratio
-//            tickIndex++;
-//            if (tickIndex > 1) {
 
-                // Save the current fps
-                that.__currentFps.update = that.__average(that.__averages.fps.update, that.__updateCount * 2);
-                that.__currentFps.render = that.__average(that.__averages.fps.render, that.__renderCount * 2);
-                that.__updateCount = 0;
-                that.__renderCount = 0;
-
-                tickIndex = 0;
-//            }
+            // Save the current fps
+            that._currentFps.update = that._average(that._averages.fps.update, that._updateCount * 2);
+            that._currentFps.render = that._average(that._averages.fps.render, that._renderCount * 2);
+            that._updateCount = 0;
+            that._renderCount = 0;
 
         }
 
         // Reset
-        this.__reset();
-        this.__isRunning = true;
+        this._reset();
+        this._isRunning = true;
 
         var now = Date.now();
-        this.__lastTickTime = now;
-        this.__lastFrameTime = now;
+        this._lastTickTime = now;
+        this._lastFrameTime = now;
 
         // Init ticking
         frameTick();
         intervalTick();
-        this.__intervalId = setInterval(intervalTick, 500);
+        this._intervalId = setInterval(intervalTick, 500);
 
         return false;
 
     },
 
     /**
-      *  Increases the pause counter.
+      * Increases the pause counter.
       *
-      *  When the pause counter is greater than 0, the time is
-      *  paused and only render will be performed per second.
-      *
-      *  @returns {Boolean} True in case the called paused the loop.
+      * When the pause counter is greater than 0, the time is
+      * paused and only render will be performed per second.
       */
     pause: function() {
-        this.__pauseCount++;
+        this._pauseCount++;
     },
 
     /**
-      *  Decreases the pause counter.
+      * Decreases the pause counter.
       *
-      *  When the pause counter is greater than 0, the time is
-      *  paused and only render will be performed per second.
-      *
-      *  @returns {Boolean} True in case the called unpaused the loop.
+      * When the pause counter is greater than 0, the time is
+      * paused and only render will be performed per second.
       */
     resume: function() {
-        this.__pauseCount = Math.max(this.__pauseCount - 1, 0);
+        this._pauseCount = Math.max(this._pauseCount - 1, 0);
     },
 
     /**
-      *  Returns whether or not the time is currently freezed.
-      *
-      *  @returns {Boolean} True in case the time is standing still.
-      */
-    isPaused: function() {
-        return this.__pauseCount > 0;
-    },
-
-    /**
-      *  Stops the loop in case it is running.
-      *
-      *  @returns {Boolean} True in case this call stopped the loop.
+      * {Boolean} Stops the loop in case it is running, true is returned
+      * in case this call stopped the loop.
       */
     stop: function() {
 
-        if (!this.__isRunning) {
+        if (!this._isRunning) {
             return false;
         }
 
-        this.__reset();
-        clearInterval(this.__intervalId);
-        this.__intervalId = -1;
+        this._reset();
+        clearInterval(this._intervalId);
+        this._intervalId = -1;
 
         return true;
 
     },
 
+
+    // Getter / Setter --------------------------------------------------------
+
+
+    /** {Boolean} Returns `true` in case the loop is running. */
     isRunning: function() {
-        return this.__isRunning;
+        return this._isRunning;
     },
 
-    // Private API
-    __reset: function() {
+    /** {Boolean} Returns whether or not the time is currently paused. */
+    isPaused: function() {
+        return this._pauseCount > 0;
+    },
 
-        this.__lastFrameTime = 0;
-        this.__lastTickTime = 0;
-        this.__tickDuration = 0;
+    /**
+      * Set the desired maximum frame rate for the update and render loop.
+      *
+      * @updateFps {Number} The desired logic updates per second.
+      * @renderFps {Number} The desired renderings per second.
+      */
+    setFps: function(updateFps, renderFps) {
 
-        this.__isRunning = false;
-        this.__pauseCount = 0;
+        this._updateFps = updateFps;
+        this._renderFps = renderFps;
 
-        this.__updateCount = 0,
-        this.__updateTime = 0;
-        this.__passedUpdateTime = 0;
-        this.__updateError = 0;
+        // We ceil / floor here so that we ensure the desired framerate in the
+        // case that we're cappping
+        this._updateDuration = Math.ceil(1000 / updateFps);
+        this._renderDuration = Math.floor(1000 / renderFps);
+        this._tickDuration = Math.max(this._updateDuration, this._renderDuration);
 
-        this.__renderCount = 0;
-        this.__renderTime = 0;
-        this.__passedRenderTime = 0;
+    },
 
-        this.__currentFps.update = 0;
-        this.__currentFps.render = 0;
+    /**
+      * {Object} Returns the fps for the last second.
+      *
+      *     { update: {Number}, render: {Number} }
+      *
+      */
+    getFps: function() {
+        return this._currentFps;
+    },
 
-        this.__averages = {
+    /**
+      * {Object} Returns the load factor for the last second.
+      *
+      *     { update: {Number}, render: {Number} }
+      *
+      */
+    getLoad: function() {
+        return this._currentLoad;
+    },
+
+    /**
+      * {Number} Returns current update time in milliseconds.
+      *
+      * Resolution is dependent on the frame rate unless @smooth {Boolean}
+      * is `true`.
+      */
+    getTime: function(smooth) {
+        return smooth ? this._passedUpdateTime : this._updateTime;
+    },
+
+
+    // Abstract methods -------------------------------------------------------
+
+    /**
+      * The update callback.
+      *
+      * @t {Integer} is the current update time.
+      *
+      * #abstract
+      */
+    update: function(t) {
+
+    },
+
+    /**
+      * The render callback.
+      *
+      * @t {Integer} is the current render time,
+      * @dt {Integer} the time passed since the last call to {Twist#update}.
+      * And @u {Float} is a represensation of @dt in the range of `0...1`
+      *
+      * #abstract
+      */
+    render: function(t, dt, u) {
+
+    },
+
+    // Private Methods --------------------------------------------------------
+    _reset: function() {
+
+        this._lastFrameTime = 0;
+        this._lastTickTime = 0;
+        this._tickDuration = 0;
+
+        this._isRunning = false;
+        this._pauseCount = 0;
+
+        this._updateCount = 0,
+        this._updateTime = 0;
+        this._passedUpdateTime = 0;
+        this._updateError = 0;
+
+        this._renderCount = 0;
+        this._renderTime = 0;
+        this._passedRenderTime = 0;
+
+        this._currentFps.update = 0;
+        this._currentFps.render = 0;
+
+        this._averages = {
 
             load: {
                 update: [],
@@ -272,114 +280,96 @@ var Twist = Class({
 
         };
 
-        this.__averageTick = 0;
-        this.__averageScale = 5;
+        this._averageTick = 0;
+        this._averageScale = 5;
 
-        this.__currentLoad.update = 0;
-        this.__currentLoad.render = 0;
+        this._currentLoad.update = 0;
+        this._currentLoad.render = 0;
 
     },
 
-    __average: function(average, value) {
+    _average: function(average, value) {
 
-        average[this.__averageTick % this.__averageScale] = value;
+        average[this._averageTick % this._averageScale] = value;
 
         var a = 0;
-        for(var i = 0; i < this.__averageScale; i++) {
+        for(var i = 0; i < this._averageScale; i++) {
             a += average[i] !== undefined ? average[i] : 0;
         }
 
-        return Math.round((a / this.__averageScale) * 100) / 100;
+        return Math.round((a / this._averageScale) * 100) / 100;
 
     },
 
-    __tick: function(doRender) {
+    _tick: function() {
 
         var beforeTick = Date.now(),
-            diff = beforeTick - this.__lastTickTime,
+            diff = beforeTick - this._lastTickTime,
             usedUpdate;
 
         // Increase game time, but limit it to 2000ms at once
         // this prevents hard freezes due to many many frames being
         // updates at once
-        if (this.__pauseCount === 0) {
-            this.__passedUpdateTime += Math.min(diff, 2000);
-            this.__passedRenderTime += Math.min(diff, 2000);
+        if (this._pauseCount === 0) {
+            this._passedUpdateTime += Math.min(diff, 2000);
+            this._passedRenderTime += Math.min(diff, 2000);
         }
 
-        while(this.__updateTime < this.__passedUpdateTime) {
+        while(this._updateTime < this._passedUpdateTime) {
 
-//            TODO limiting hard here brings jumps in the updates with it
-//            this shows lag when simulating with Box2D
-//            if (this.__updateCount < this.__updateFps) {
-                this.update(this.__updateTime, this.__updateDuration);
-                this.__updateCount++;
-//            }
+            // Limiting the update rate might be bad in case you're running a
+            // physics simulation.
+            if (!this._frameCap || this._updateCount < this._updateFps) {
+                this.update(this._updateTime, this._updateDuration);
+                this._updateCount++;
+            }
 
-            this.__updateTime += this.__updateDuration;
+            this._updateTime += this._updateDuration;
 
         }
 
         usedUpdate = Date.now() - beforeTick;
 
         // Render Fps handling
-        var oldRenderTime = this.__renderTime,
+        var oldRenderTime = this._renderTime,
             beforeRender,
             usedRender;
 
-        while(this.__renderTime < this.__passedRenderTime) {
-            this.__renderTime += this.__renderDuration;
+        while(this._renderTime < this._passedRenderTime) {
+            this._renderTime += this._renderDuration;
         }
 
         // Only render in case its actually necessary
         beforeRender = Date.now();
-        if (doRender && (this.__renderTime > oldRenderTime || this.__pauseCount !== 0) && this.__renderCount < this.__renderFps) {
+        if ((this._renderTime > oldRenderTime || this._pauseCount !== 0)
+            && this._renderCount < this._renderFps) {
 
-            var rdt = this.__renderTime - this.__updateTime;
-            this.render(this.__renderTime, rdt, rdt / this.__updateDuration);
-            this.__renderCount++;
+            var rdt = this._renderTime - this._updateTime;
+            this.render(this._renderTime, rdt, rdt / this._updateDuration);
+            this._renderCount++;
 
         }
 
         usedRender = Date.now() - beforeRender;
 
         // Calculate the load
-        this.__currentLoad.update = this.__average(this.__averages.load.update, usedUpdate <= 0 ? 0 : usedUpdate / this.__updateDuration);
-        this.__currentLoad.render = this.__average(this.__averages.load.render, usedRender <= 0 ? 0 : usedRender / this.__renderDuration);
+        this._currentLoad.update = this._average(this._averages.load.update, usedUpdate <= 0 ? 0 : usedUpdate / this._updateDuration);
+        this._currentLoad.render = this._average(this._averages.load.render, usedRender <= 0 ? 0 : usedRender / this._renderDuration);
 
-        if (this.__averageTick++ > this.__averageScale) {
-            this.__averageTick = 0;
+        if (this._averageTick++ > this._averageScale) {
+            this._averageTick = 0;
         }
 
         // Make sure the interval is somewhat stable
         var used = Date.now() - beforeTick;
-        this.__lastTickTime = beforeTick - used;
+        this._lastTickTime = beforeTick - used;
 
-    },
-
-    // Abstract methods -------------------------------------------------------
-    // ------------------------------------------------------------------------
-
-    /**
-      *  The update callback.
-      *
-      *  @param {Number} t The current update time.
-      */
-    update: function(t) {
-    },
-
-    /**
-      *  The render callback.
-      *
-      *  @param {Number} t The current render time.
-      */
-    render: function(t) {
     }
 
 });
 
 
-window.requestAnimFrame = (function() {
+window.twistAnimationFrame = (function() {
 
     return window.requestAnimationFrame || window.webkitRequestAnimationFrame ||
            window.mozRequestAnimationFrame || window.oRequestAnimationFrame ||
